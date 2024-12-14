78ca28f617c16f98a103e07443e9e2408421ef14
anakon <nazarkinah73@gmail.com>
2024-12-14 13:25:10 +0000

    feat(MeetingService): use api

--- src/pages/MeetingService/MeetingService.tsx
+++ src/pages/MeetingService/MeetingService.tsx
@@ -1,14 +1,45 @@
+import { useServices } from '@/shared/api/services'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { useMemo, useState } from 'react'
 
 export function MeetingService() {
+  const { data } = useServices()
+  const [index, setIndex] = useState(0)
+
+  const currentService = useMemo(() => {
+    if (data)
+      return data[index]
+
+    return null
+  }, [data, index])
+
+  if (currentService === null) {
+    return (
+      <div>no service</div>
+    )
+  }
+
   return (
-    <div className="mx-auto w-full max-w-[700px] font-medium">
+    <div className="mx-auto w-full max-w-[700px] pt-5 font-medium">
       <div className="flex flex-col items-center font-bold">
-        <p>На неделе от</p>
-        <div className="mt-2 flex items-center">
-          <ChevronLeft className="size-6" />
-          <p>11.12.2024</p>
-          <ChevronRight className="size-6" />
+        <div className="flex items-center">
+          <button
+            disabled={index === 0}
+            onClick={() => setIndex(prev => prev - 1)}
+          >
+            <ChevronLeft className="size-5" />
+          </button>
+          <p>
+            {
+              currentService.date.toLocaleString(
+                'ru',
+                { day: 'numeric', month: 'numeric', year: 'numeric' },
+              )
+            }
+          </p>
+          <button disabled={index === data!.length - 1} onClick={() => setIndex(prev => prev + 1)}>
+            <ChevronRight className="size-5" />
+          </button>
         </div>
       </div>
       <div className={`
@@ -19,19 +50,19 @@ export function MeetingService() {
       >
         <div className="flex justify-between rounded-xl bg-white drop-shadow-mainshadow">
           <p className="px-3 py-1">Сцена</p>
-          <p className="px-3 py-1 text-right">Мостовой Максим</p>
+          <p className="px-3 py-1 text-right">{currentService.scene}</p>
         </div>
         <div className="flex justify-between rounded-xl bg-white drop-shadow-mainshadow">
           <p className="px-3 py-1">Микрофоны</p>
-          <p className="px-3 py-1 text-right">Данов Илья & Шилюк Арсений</p>
+          <p className="px-3 py-1 text-right">{currentService.microphones}</p>
         </div>
         <div className="flex justify-between rounded-xl bg-white drop-shadow-mainshadow">
           <p className="px-3 py-1">Озвучивание + зум</p>
-          <p className="px-3 py-1 text-right">Братья с собрания Тринец</p>
+          <p className="px-3 py-1 text-right">{currentService.voiceoverZoom}</p>
         </div>
         <div className="flex justify-between rounded-xl bg-white drop-shadow-mainshadow">
           <p className="px-3 py-1">Распорядители</p>
-          <p className="px-3 py-1 text-right">Брат с собрания Тринец & Мостовой Максим</p>
+          <p className="px-3 py-1 text-right">{currentService.administrator}</p>
         </div>
       </div>
     </div>
