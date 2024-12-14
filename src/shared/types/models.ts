151e409a59e68f9887797156abd2a61cc86f06e7
anakon <nazarkinah73@gmail.com>
2024-12-14 13:23:07 +0000

    feat(models)

--- src/shared/types/models.ts
+++ src/shared/types/models.ts
@@ -6,6 +6,7 @@ export interface Address {
 export interface Services {
   id: number
   scene: string
+  date: Date
   microphones: string
   voiceoverZoom: string
   administrator: string
@@ -13,13 +14,39 @@ export interface Services {
 
 export interface Meetings {
   id: number
+  date: Date
   leading: string
-  speaker: string
-  speechTitle: string
-  leadWt: string
-  reader: string
+  speaker: string | null
+  speechTitle: string | null
+  leadWt: string | null
+  reader: string | null
   closingPrayer: string
-  specialProgram: string
-  placeAddress: string
-  placeUrl: string
+  specialProgram: string | null
+  address: Address
+  status: {
+    id: number
+    title: string
+  }
+  ministryMeeting: {
+    id: number
+    date: Date
+    leader: string
+  }
+}
+
+export interface FriendlyMeeting {
+  id: number
+  date: Date
+  description: string
+  inviting: string
+  address: string
+  addressUrl: string
+}
+
+export interface MinistryMeeting {
+  id: number
+  date: Date
+  leader: string
+  address: Address
+  friendlyMeeting: FriendlyMeeting
 }
